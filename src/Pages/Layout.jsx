a3c700a50b629f625a94eb547901277d79ebcc5d
KalviKal <66356112+KalviKal@users.noreply.github.com>
2025-05-16 19:41:49 +0300

    Loeng6

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,6 +1,26 @@
+import { Outlet, NavLink } from "react-router-dom";
+
 function Layout(){
     return(
-        <h1>Siia tuleb üldine layout</h1>
+        <div className="container">
+            <header>
+                <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                        <a className="navbar-brand" href="#">Kellatöökoda</a>
+                        </div>
+                        <ul className="nav navbar-nav">
+                        
+                        <li><NavLink to="/">Esileht</NavLink></li>
+                        <li><NavLink to="/news">Uudised</NavLink></li>
+                        <li><NavLink to="/contact">Kontakt</NavLink></li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+                <Outlet />
+            <footer>jalus</footer>
+        </div>
     )
 }
 
